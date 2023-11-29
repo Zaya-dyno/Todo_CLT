992f7bd91de99d192e562be489b28ce284ae6ba0
tuvshinzaya erdenekhuu <tuvshinzaya.erdenekhuu@arm.com>
2023-11-29 10:50:06 +0000

    added __str__ function for Task class

--- src/task.py
+++ src/task.py
@@ -1,6 +1,6 @@
 class RepTask:
     Title:str
-    Repeated
+    Repeated:str
     Tags:[str]
 
     def __init__(title,repeated,tags):
@@ -9,11 +9,20 @@ class RepTask:
         self.Tags = tags
 
 class Task:
+    ID:int
     Title:str
     Done:bool
     Tags:[str]
 
-    def __init__(title,done,tags):
+    def __init__(self,iden,title,done,tags):
+        self.ID = iden 
         self.Title = title
         self.Done = done
         self.Tags = tags
+    
+    def __str__(self):
+        ret = "{"
+        ret += "ID:{},Title:\"{}\",done:{},tags:".format(self.ID,self.Title,self.Done)
+        ret += self.Tags.__str__()
+        ret += "}"
+        return ret
