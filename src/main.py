e6051e16e4a9ce24afde9e62c576d0007cff7f60
tuvshinzaya erdenekhuu <tuvshinzaya.erdenekhuu@arm.com>
2023-11-29 14:44:03 +0000

    added logging

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 from todo import Todo
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    filename="data/log",
+                    filemode="w")
 
 if __name__ == "__main__":
     todo = Todo()
